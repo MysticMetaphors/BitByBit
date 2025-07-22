cb2d9e4dab7cbd4e59c5b3cfb8f09078c87390db
VonBryan <vonbanalbryan18v@gmail.com>
2025-07-23 06:30:36 +0800

    CrreatorsWorks

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,10 @@ Route::get('/ai', function () {
     return Inertia::render('AI');
 })->name('ai');
 
+Route::get('/works', function () {
+    return Inertia::render('CreatorsWorks');
+})->name('works');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
